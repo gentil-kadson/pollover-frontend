5fbc9d434c11b63ec131d669b75a99c2d9a67087
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-07-30 10:22:27 -0300

    adds possibility to add more poll options.

--- src/pages/create-poll.tsx
+++ src/pages/create-poll.tsx
@@ -1,8 +1,32 @@
 import styles from "../styles/CreatePoll.module.css";
 
 import PollOption from "@/components/PollOption";
+import { useState } from "react";
 
 export default function CreatePoll() {
+  const [options, setOptions] = useState<JSX.Element[]>([
+    <PollOption optionNumber={1} key={1} />,
+    <PollOption optionNumber={2} key={2} />,
+  ]);
+
+  function handleAddOption() {
+    const index = options.length + 1;
+    const newOption = <PollOption optionNumber={index} key={index} />;
+    setOptions((prevState) => {
+      const updatedOptions = [...prevState];
+      updatedOptions.push(newOption);
+      return updatedOptions;
+    });
+  }
+
+  function handleLastOptionRemoval() {
+    setOptions((prevState) => {
+      const updatedOptions = [...prevState];
+      updatedOptions.pop();
+      return updatedOptions;
+    });
+  }
+
   return (
     <main className={styles.main}>
       <section className={styles.section}>
@@ -19,14 +43,22 @@ export default function CreatePoll() {
               id="pollTitle"
             />
           </div>
-          <section>
-            <PollOption optionNumber={1} />
-            <PollOption optionNumber={2} />
+          <section className={styles.pollOptions}>
+            {options.map((option) => option)}
           </section>
         </form>
-        <div>
-          <button>Add option</button>
-          <button>Save</button>
+        <div className={styles.buttonsContainer}>
+          <button className={styles.button} onClick={handleAddOption}>
+            Add option
+          </button>
+          <button
+            disabled={options.length == 2 ? true : false}
+            className={styles.button}
+            onClick={handleLastOptionRemoval}
+          >
+            Remove last option
+          </button>
+          <button className={styles.button}>Save</button>
         </div>
       </section>
     </main>
